bdf12f8c849a5f83022a141e32d6cb4325abff4d
Mattuser <75076355+Mattuser@users.noreply.github.com>
2022-04-08 19:28:27 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -12,14 +12,23 @@ def main():
     return render_template('index.html')
 
 
-@app.route('/calc', methods=["GET","POST"])
-def calc():
+@app.route('/calcular', methods=["GET","POST"])
+def calcular():
     n1 = request.form['firstn']
     n2 = request.form['secondn']
     op = request.form['op']
     
     if op == 'soma':
         result = int(n1) + int(n2)
+    if op == 'divisão':
+        try:
+            result = int(n1) * int(n2)
+        except ZeroDivisionError:
+            result = 0
+    if op == 'multiplicação':
+        result = int(n1) * int(n2)
+    if op == 'subtração':
+        result = int(n1) - int(n2)
     
     print(n1)
     return str(result)
@@ -27,3 +36,4 @@ def calc():
 if __name__ == '__main__':
    app.run(debug=True, host='localhost', port=5000)
 
+ 
